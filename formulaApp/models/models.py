b3dfff2b5b87658ab1417bc88fe9c5e076bd9828
BeratBurakTetik <63604101+BeratBurakTetik@users.noreply.github.com>
2022-03-13 23:18:15 +0300

    make some properties nullable and migrate

--- formulaApp/models/models.py
+++ formulaApp/models/models.py
@@ -6,9 +6,9 @@ from pyexpat import model
 from statistics import mode
 
 class Driver(models.Model):
-    driverRef = models.CharField(max_length=50)
-    number = models.IntegerField()
-    code = models.CharField(max_length=10)
+    driverRef = models.CharField(max_length=50, null=True, blank=True)
+    number = models.IntegerField(null=True, blank=True)
+    code = models.CharField(max_length=10, null=True, blank=True)
     forename = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     dob = models.DateField()
@@ -16,19 +16,19 @@ class Driver(models.Model):
     url = models.CharField(max_length=100)
 
 class Constructor(models.Model):
-    constructorRef = models.CharField(max_length=50)
+    constructorRef = models.CharField(max_length=50, null=True)
     name = models.CharField(max_length=40)
     nationality = models.CharField(max_length=40)
     url = models.CharField(max_length=100)
 
 class Circuit(models.Model):
-    circuitRef = models.CharField(max_length=50)
+    circuitRef = models.CharField(max_length=50 ,null=True)
     name = models.CharField(max_length=80)
     location = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
-    lat = models.DecimalField(decimal_places=5 , max_digits=10)
-    lng = models.DecimalField(decimal_places=5 , max_digits=10)
-    alt = models.DecimalField(decimal_places=5 , max_digits=10)
+    lat = models.DecimalField(decimal_places=5 , max_digits=10,null=True, blank=True)
+    lng = models.DecimalField(decimal_places=5 , max_digits=10,null=True, blank=True)
+    alt = models.DecimalField(decimal_places=5 , max_digits=10,null=True, blank=True)
     url = models.CharField(max_length=100)
 
 
