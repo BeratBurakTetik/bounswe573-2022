c6680d279af3bbf0cda7efca1f3eb3d63e22dce4
BeratBurakTetik <63604101+BeratBurakTetik@users.noreply.github.com>
2022-04-24 15:38:04 +0300

    configure settings.py for postgreSql

--- anyLearnApp/anyLearnApp_Web/anyLearnApp/settings.py
+++ anyLearnApp/anyLearnApp_Web/anyLearnApp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'anyLearnApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'anyLearnDB', 
+        'USER': 'postgres', 
+        'PASSWORD': 'arda66.',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
